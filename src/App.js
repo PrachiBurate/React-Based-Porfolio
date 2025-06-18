f0105d96078bcd9736e446543eab69c30766bab5
Prachi Ramesh Burate <125243122+PrachiBurate@users.noreply.github.com>
2025-06-18 22:42:16 +0530

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import "./App.css";
 function App() {
-   const test = (a) => {
+  const test = (a) => {
     alert(a);
   };
   return (
-    
     <>
       <section>
         <div id="main-div">
@@ -27,7 +26,7 @@ function App() {
         </div>
         <div id="buttonDiv">
           <button id="button1">MORE ABOUT ME</button>
-          <button id="button2" onClick={()=>test('test test')}>GET IN TOUCH</button>
+          <button id="button2">GET IN TOUCH</button>
           <h1 id="dash">__________________</h1>
           <button id="button3">GET MY CV</button>
         </div>
@@ -41,6 +40,11 @@ function App() {
             <li></li>
           </ul>
         </div>
+
+        <br />
+        <br />
+        <br />
+        <br />
 
         <div>
           <div
@@ -69,18 +73,124 @@ function App() {
             About Me.
           </div>
         </div>
-        
+
         <div id="about-text">
-            <br/>
-          A Product Designer & Developer.
-          <br/>
-          <div>The sky was painted with shades of  <br/>orange and pink as the sun began to set. <br/> A gentle breeze rustled the leaves,  <br/>and birds chirped their final songs of the day. </div>
+          <div>
+            Lorem ipsum dolor sit amet consectetur <br />
+            adipisicing elit.
+            <br />
+            Voluptas explicabo id debitis <br /> cupiditate cumque, error quia!{" "}
+            <br />
+            Debitis cupiditate ipsa nihil labore, <br /> earum atque dolor quos
+            saepe vero inventore, <br />
+            odit nam deleniti accusantium omnis mollitia <br />
+            consequatur harum incidunt! Ex modi aut <br /> repudiandae
+            doloremque commodi <br />
+            recusandae labore sit.
+          </div>
         </div>
 
-        <div>
-            <div id="about1"></div>
-            <div id="about2"></div>
+        <div style={{ paddingBottom: "300px" }}>
+          <div id="about1"></div>
+          <div id="about2"></div>
+          <div id="about3"></div>
+          <div id="about4"></div>
+          <div id="about5"></div>
+          <div id="about6"></div>
         </div>
+
+        <div id="section3">
+          <div id="experiance" class="sec" style={{ marginLeft: "150px" }}>
+            <ul>
+              <li class="list-head" >
+                <h3><span class="span">|</span> Experiance</h3>
+              </li>
+                <br/>
+              <li class="list">
+                <h4>Spotify</h4>
+                <p>Product Designer</p>
+              </li>
+              <li class="list">
+                <h4>Dropbox</h4>
+                <p>Interface Developer</p>
+              </li>
+              <li class="list">
+                <h4>Google</h4>
+                <p>Lead UI Designer</p>
+              </li>
+              <li class="list">
+                <h4>Figma</h4>
+                <p>UI Designer</p>
+              </li>
+              <li class="list">
+                <h4>Microsoft</h4>
+                <p>UI Designer</p>
+              </li>
+            </ul>
+          </div>
+          <div id="award" class="sec">
+            <ul>
+              <li class="list-head">
+                <h3><span class="span"> |</span> Award</h3>
+              </li>
+                <br/>
+              <li class="list">
+                <h4>Site Of The Month</h4>
+                <p>Awwwards — 2023</p>
+              </li>
+              <li class="list">
+                <h4>Site Of The Day</h4>
+                <p>Awwwards — 2023</p>
+              </li>
+              <li class="list">
+                <h4>Agency of The Year</h4>
+                <p>Awwwards — 2022</p>
+              </li>
+              <li class="list">
+                <h4>FWA of The Month</h4>
+                <p>FWA — 2022</p>
+              </li>
+              <li class="list">
+                <h4>Site Of The Month</h4>
+                <p>Awwwards — 2022</p>
+              </li>
+            </ul>
+          </div>
+          <div id="skill" class="sec">
+            <ul>
+              <li class="list-head">
+                <h3><span class="span">|</span> Skill</h3>
+              </li>
+              <br/>
+                <li class="list">
+                <h4>Product Design</h4>
+              </li>
+              <li class="list">
+                <h4>Prototyping</h4>
+               
+              </li>
+              <li class="list">
+                <h4>UI/UX Design</h4>
+              
+              </li>
+              <li class="list">
+                <h4>Frontend Development</h4>
+              
+              </li>
+              <li class="list">
+                <h4>Illustration</h4>
+              
+              </li>
+                 <li class="list">
+                <h4>Visual Design</h4>
+              
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div id="section4"></div>
+
+        <div></div>
       </section>
     </>
   );
